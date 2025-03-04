18467804d4af7744e1beab01a07ac689ea6fa333
Tamas Szuromi <tromika@gmail.com>
2025-03-04 16:34:23 +0100

    make it a template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
+import { X } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="flex items-center gap-2">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <span>
+            <X />
+          </span>
+          <Image
+            className="dark:invert"
+            src="/codeanywhere-logo-full-color.svg"
+            alt="Codeanywhere logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -25,23 +39,23 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#8366D8] text-background gap-2 hover:bg-[#0BCE9A] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://app.codeanywhere.com?utm_source=codeanywhere-nextjs-template"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/codeanywhere-logo-white.svg"
+              alt="Codeanywhere logomark"
               width={20}
               height={20}
             />
-            Deploy now
+            Dashboard
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://codeanywhere.com/docs/?utm_source=codeanywhere-nextjs-template"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -63,7 +77,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Learn Next.js
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +92,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Next.js Examples
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
